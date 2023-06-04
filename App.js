1d12ebc7a677677eb5489db3f11201b360b00c2b
Jack <jack.k.garritano@vanderbilt.edu>
2023-06-03 22:09:30 -0500

    initial commit

--- App.js
+++ App.js
@@ -1,10 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { useState } from 'react';
+
+function AddButton({count, setCount}){
+  function onClick(){
+    setCount(count + 1);
+  }
+  return (<Button title='Add' onPress = {onClick} />);
+}
+
+function SubtractButton({count, setCount}){
+  function onClick(){
+    setCount(count - 1);
+  }
+  return (<Button title='Subtract' onPress = {onClick} />);
+}
 
 export default function App() {
+  let [count, setCount] = useState(0);
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Current count: {count}</Text>
+      <View style={styles.buttonHolder}>
+        <View style={styles.incButton}><AddButton count={count} setCount={setCount} /></View>
+        <View style={styles.incButton}><SubtractButton count={count} setCount={setCount} /></View>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +38,18 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  buttonHolder: {
+    display: 'flex',
+    justifyContent: 'center',
+    flexDirection: 'row',
+  },
+  incButton: {
+    backgroundColor: '#ccc',
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: 'black',
+    borderRadius: 5,
+    margin: 10,
   },
 });
